a309095fd5a33eabbf5482998ba4d465210d81ac
alzarough <alzarough@gmail.com>
2021-10-17 06:17:07 +0200

    Setup correct group readings

--- custom/io_mia_hr/model/hr_attendance.py
+++ custom/io_mia_hr/model/hr_attendance.py
@@ -54,7 +54,7 @@ class HrAttendance(models.Model):
             hour = d1.hour
             minute = d1.minute
             if hour == 9:
-                if minute <= 1:
+                if minute <= 11:
                     self.status = 'present'
                 else:
                     self.status = 'late'
