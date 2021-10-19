0fc75f8e56fcafa05cdb2a1207c0973a45160f42
alzarough1106 <92651632+alzarough1106@users.noreply.github.com>
2021-10-19 20:49:14 +0200

    Delete custom/loza_account directory

--- custom/loza_account/model/loza_payment_category.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api, _
-
-
-class loza_payment_category(models.Model):
-    _name = "loza.payment.category"
-    _order = "name desc"
-
-    name = fields.Char(string='الإسم', copy=False, required=True)
